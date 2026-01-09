89b9e9d9fc7e9b9a76196e666934bd20bd3ef767
CVDev-code <4bztqh69hn@privaterelay.appleid.com>
2026-01-09 22:18:54 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="O-1 PDF Highlighter")
+
+st.title("O-1 PDF Highlighter")
+st.write("If you can see this, your app.py is wired correctly 🎉")
+
+st.info("Next step: we will add PDF upload + highlighting logic.")
